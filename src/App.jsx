60309a60eba4d82a982decb735b36aa927fc4a7c
Muxammadbobur <bobur0172@gmail.com>
2023-03-15 10:47:19 -0700

    add toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react";
 import "./App.css";
-const tg = window.Telegram.WebApp;
+import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
+  const { onToggleButton, tg } = useTelegram();
+
   useEffect(() => {
     tg.ready();
-  });
+  }, []);
 
   return (
     <div className="App">
-      Hello World
-      <button onClick={onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
 }
