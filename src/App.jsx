9b2d487834b9cb85fb203a021f91b73ef0f014e0
Muxammadbobur <bobur0172@gmail.com>
2023-03-15 10:51:52 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={onToggleButton}>Toggle</button>
+      <button onClick={onToggleButton}>Toggle New</button>
     </div>
   );
 }
