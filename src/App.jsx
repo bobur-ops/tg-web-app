1696c9c4152f3f4f8d863c489e0f084b5187137a
Muxammadbobur <bobur0172@gmail.com>
2023-03-15 11:28:30 -0700

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import { useTelegram } from "./hooks/useTelegram";
+import ProductList from "./components/Productlist/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
@@ -13,7 +16,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>Toggle New</button>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path="form" element={<Form />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Productlist/ProductList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProductList.css";
+
+const Productlist = () => {
+  return <div>Productlist</div>;
+};
+
+export default Productlist;
